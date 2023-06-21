4528d1274b0d6850db73473f91aa8bea54d9f528
yehoki <patsenn2000@gmail.com>
2023-06-21 18:48:32 +0100

    Finish Register UI

--- firebnb/app/components/modals/Modal.tsx
+++ firebnb/app/components/modals/Modal.tsx
@@ -152,6 +152,7 @@ const Modal: React.FC<ModalProps> = ({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
